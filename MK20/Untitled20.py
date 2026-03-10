8cb77ec659d200b85e59b3d65389fc9f88b9f82d
MAXTIT73 <mkosanovsky@gmail.com>
2026-03-10 16:30:31 +0100

    Add files via upload

---
+++ MK20/Untitled20.py
@@ -0,0 +1,34 @@
+#import string
+#import random
+
+#def funkcja():
+#    """opis funkcji:"""
+
+#cośtam = "cośtam"
+#cośjeszcze = "cośjeszcze"
+#wiekkkkk = "wiekkkkk"
+
+#print (f"coś tam {wiekkkkk}", "i coś jeszcze")
+
+#print(string.punctuation)
+
+
+#znaki = string.ascii_uppercase + string.digits
+
+#print("LU",(random.choice(znaki)),(random.choice(znaki)),(random.choice(znaki)),(random.choice(znaki)),(random.choice(znaki)))
+
+#a = input("podaj tekst do szyfrowania ")
+#b = int(input("podaj klucz "))
+
+#tekst = "A"
+#klucz = 4
+
+def szyfr(tekst, klucz):
+    for x in tekst:
+        kod = ord(x)
+        htena = chr(kod + klucz)
+        zaszyfrowany += litera
+        return zaszyfrowany
+
+print(chr(92))
+
